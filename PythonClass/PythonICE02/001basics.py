6b3f96a064e7e162119b135b536050378f5e6c33
Aadesh <aadeshlokhande11@gmail.com>
2023-02-27 22:08:35 +0530

    practice set1

---
+++ PythonClass/PythonICE02/001basics.py
@@ -0,0 +1,42 @@
+# print(23,12,43.54,"hello",sep="-")
+
+# print("Hello")
+# print("World")
+# escape char
+# \n = new line 
+# \t = horizontal tab
+
+# print("Hello \nw\torld")
+# print("Hello",end="")
+# print("World")
+
+
+# keywords - reserve words
+# import keyword
+# print(keyword.kwlist)
+
+
+# a = ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with','yield']
+# print(len(a))
+
+
+# comments - notes
+
+# printing hello world
+# print("Hello world")
+
+# a = """
+# printing hello world
+# printing hello world
+# printing hello world
+# printing hello world
+# printing hello world
+# """
+# print(type(a))
+
+
+# tokens - smallest element
+# print("Hello world")
+
+
+# 7058 232 826 - Aadesh Lokhande 
