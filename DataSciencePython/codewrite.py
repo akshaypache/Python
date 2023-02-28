02e6f7fca5a28a3a1cee121780d99adde0b3fc6c
Aadesh <aadeshlokhande11@gmail.com>
2023-02-28 23:18:48 +0530

    practice 2

--- DataSciencePython/codewrite.py
+++ DataSciencePython/codewrite.py
@@ -9,14 +9,3 @@ file.close()
 for i in data:
     pg.typewrite(i)
     pg.hotkey("shift","enter")
-
-
-
-
-# for i in range(7):
-#     for j in range(7):
-#         if abs(i-3) + abs(j-3) <= 3:
-#             print(5 - abs(i-3) - abs(j-3), end="")
-#         else:
-#             print(end=" ")
-#     print()
