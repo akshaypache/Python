3d22c8941d3c268c16854fa82cb44685022a353a
Aadesh <aadeshlokhande11@gmail.com>
2023-03-03 00:45:32 +0530

    list

---
+++ DataSciencePython/Practice/lisCo.py
@@ -0,0 +1,44 @@
+# l = []
+# for i in range(1,11):
+#     l.append(i)
+# print(l)
+
+# list = [i for i in range(1,21,2)]
+# print(list)
+
+# a = int(input("Value de be = "))
+# list = [i*a for i in range(1,11)]
+# print(list)
+
+
+# list = [i for i in range(1,21) if i%2!=0]
+# print(list)
+
+# list = []
+# for i in range(1,21):
+#     if(i%2==0):
+#         list.append("Aadesh")
+#
+# print(list)
+
+
+
+
+
+
+# operator
+# string
+# list
+# tuple
+# dic
+# set
+# condition
+# loop
+#     for
+#             nested for loop
+#     while
+
+
+
+# function
+# inline function : lambda
