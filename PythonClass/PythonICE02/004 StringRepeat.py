2bc76e51c0948cdf764cd3731be8ba15800c2baa
Aadesh <aadeshlokhande11@gmail.com>
2023-03-18 17:49:27 +0530

    string

---
+++ PythonClass/PythonICE02/004 StringRepeat.py
@@ -0,0 +1,76 @@
+# slicing
+# a = "bhai kaise ho"
+
+# print(len(a))
+# print(type(a))
+# print(a[-5])
+# print(a[5])
+# print(a[5:10])
+# print(a[:10])
+# print(a[5:])
+# print(a[:])
+# print(a)
+# print(a[-13:-9])
+# print(a[5:10:1])
+# print(a[5:10:2])
+# print(a[::2])
+
+# print(a[::-3])
+# print(a[5:10:-1])
+# print(a[9:4:-1])
+# print(a[0:4][::-1])
+
+# s1 = a[0:4]
+# s2 = a[11:13]
+# s3 = s1 + " "+ s2
+# print(s1)
+
+
+# ==============================
+# Methods
+
+# a = "bhAi tUm kAiSe ho"
+# print(a)
+# print(a.upper())
+# print(dir(a))
+# print(type(a))
+# print(a.lower())
+# print(a.capitalize())
+# print(a.title())
+# print(a.find("tUm"))
+# print(a.count("A"))
+# print(a.index('h'))
+# print(a.swapcase())
+
+# =================Formating===============
+# name = input("Enter your name = ") 
+# age = int(input("Enter your age = "))
+# classs = "Python"
+# a = "My name is",name, "and my age is ",age, "year old"
+# a = "My name is {} and my age is {} year old".format(name,age)
+# a = "My name is {1} and my age is {0} year old".format(name,age)
+# a = f"My name is {name} and my age is {age} year old, classs = {classs}"
+# print(a)
+
+# msg = "I love you babu"
+# print(msg)
+# print(msg.replace("love","miss"))
+
+
+# a = "bhai kaise ho"
+# l1 = a.split('a')
+# print(l1)
+
+# print(l1[0],l1[2])
+# ===========================================
+# msg = "I love you\n"
+# num = 3000
+
+
+# print(msg * num)
+
+# print("i miss you babu\n"*10)
+
+
+
+# print(eval(input("Enter equation = ")))
